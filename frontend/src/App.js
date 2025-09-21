dcd2e9f664621dda38cedea5bf2ca738c4214211
SYED ALTHAF HUSSAIN <114658392+ALTHAFHUSSAINSYED@users.noreply.github.com>
2025-09-21 23:47:51 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
+// src/App.js
+
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Portfolio from "./components/Portfolio";
+import ProjectDetailPage from "./components/ProjectDetailPage"; // ✨ NEW: Import the new component
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          {/* This is the route for your main portfolio homepage */}
           <Route path="/" element={<Portfolio />} />
+
+          {/* ✨ NEW: This is the new route for the project details page */}
+          <Route path="/projects/:projectId" element={<ProjectDetailPage />} />
         </Routes>
       </BrowserRouter>
     </div>
